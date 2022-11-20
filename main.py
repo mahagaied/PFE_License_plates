47c3e6ae6ca916edf3551b438a938f0797d2aef2
mahag <mahagaied@gmail.com>
2022-11-20 15:02:55 +0100

    first detection

--- main.py
+++ main.py
@@ -1,43 +1,26 @@
 import cv2
 import utilities
-import imutils
+
 
 if __name__ == '__main__':
-    path = "license_images/5.png"
+    path = "license_images/3.png"
     image = cv2.imread(path)
     utilities.display_image(image)
 
     gray = utilities.RGB_to_GrayScale(image)
     utilities.display_image(gray)
 
-    """#seuillage
-    thresh = utilities.threshold(image, 130, 255)
-    utilities.display_image(thresh)
+    """ FIRST METHOD: noise reduce + canny + find contours+ assume 4 points contours
+    problem : detects the glass cleaner as a 4 point polygone
+    solution : verify the length of parallel sides to be almost equal
+    
+    2nd problem : detects another rectangle that is the lights and not the plate
+    2nd solution : another method using findpossibleCharsinScene to detect rectangles with characters in it
     """
-    # noise reduce
-    gray_filter = cv2.bilateralFilter(gray, 40, 40, 30) # 35, 25, 25 / 100, 70, 70)
-    utilities.display_image(gray_filter)
 
-    # canny : all contours
-    gray_contours = cv2.Canny(gray_filter, 30, 200)
-    utilities.display_image(gray_contours)
-
-    # find contours
-    points = cv2.findContours(gray_contours.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)  # we want 4 points
-    contours = imutils.grab_contours(points)
-    contours = sorted(contours, key=cv2.contourArea, reverse=True)[:10]
-
-    rect_contours = None  # plate contour we are looking for
-    for contour in contours:
-        # change 10 for better results
-        poly_points = cv2.approxPolyDP(contour, 10, True)  # approximate polygone, 10 fine enough for a rectangle
-        print(len(poly_points))
-        if len(poly_points) == 4:  # a rectangle has 4 key points
-            rect_contours = poly_points
-            break
-
-    print(rect_contours)
+    rect_contours = utilities.detect_polygone(gray)
 
     cv2.drawContours(image, [rect_contours], 0, (0, 255, 0), 3)
     cv2.imshow('Contours', image)
     cv2.waitKey(0)
+
--- utilities.py
+++ utilities.py
@@ -1,4 +1,5 @@
 import cv2
+import imutils
 
 
 # function that loads and displays an image
@@ -27,4 +28,71 @@ def threshold(image, th, max_th):
     (T, thresh) = cv2.threshold(grayscale, th, max_th, cv2.THRESH_BINARY_INV)
     return thresh
 
+# function that gives x, y for each point i from an numpy array
+# @input : np array with all the points, index of the point
+# @output : xi, yi
+def xy_coordinates(poly_points, i):
+
+    len_i = len(str(poly_points[i]))
+    x, y = str(poly_points[i])[2:len_i - 2].split()
+
+    return x, y
+
+# function that checks if 2 parallel sides are almost equal
+# @input : coordinates
+# @output : boolean
+def almost_equals(coord):
+    y_left = abs(coord[0][1]-coord[3][1]) # longeur verticale
+    y_right = abs(coord[1][1]-coord[2][1])
+    if y_right - 5 <= y_left <= y_right + 5 :
+        return True
+    return False
+
+
+# function that does the preprocess : noise reduce + noise reduce + canny + find contours
+# @input : gray image
+# @output : contours
+def pre_process_contours(gray):
+    # noise reduce
+    gray_filter = cv2.bilateralFilter(gray, 40, 40, 30)  # 35, 25, 25 / 100, 70, 70)
+    # gray_filter = cv2.bilateralFilter(gray, 100, 65, 65)
+    # display_image(gray_filter)
+
+    # canny : all contours
+    gray_contours = cv2.Canny(gray_filter, 30, 200)
+    # display_image(gray_contours)
+
+    # find contours
+    points = cv2.findContours(gray_contours.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)  # we want 4 points
+    contours = imutils.grab_contours(points)
+    contours = sorted(contours, key=cv2.contourArea, reverse=True)[:10]
+
+    return contours
+
+
+# function for THE FIRST METHOD: preprocess + detect 4 point polygones + choose the one with almost equal sides
+# @input : gray image
+# @output : rectangle contour
+def detect_polygone(gray):
+    # noise reduce + canny + find contours
+    contours = pre_process_contours(gray)
+
+    rect_contours = None  # plate contour we are looking for
+
+    for contour in contours:
+        # change 10 for better results
+        poly_points = cv2.approxPolyDP(contour, 10, True)  # approximate polygone, 10 fine enough for a rectangle
+        print(len(poly_points))
+        coord = []  # coordinates
+        if len(poly_points) == 4:  # a rectangle has 4 key points
+            for i in range(4):
+                x, y = xy_coordinates(poly_points, i)
+                coord.append([int(x), int(y)])
+                print(coord)
+            # check if parallel sides are almost equal
+            if almost_equals(coord):
+                rect_contours = poly_points
+                break
+
+    return rect_contours
 
